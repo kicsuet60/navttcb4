13f43e576d89e6037c3675e614289b230851439c
kicsuet60 <86164034+kicsuet60@users.noreply.github.com>
2023-06-23 17:51:11 +0500

    PHP Assignments Should Complete

---
+++ hello-world.php
@@ -0,0 +1,43 @@
+<?php 
+    // $name = "Arif";
+    // $profile = "Backend Developer";
+    // $age = 20;
+    // // echo $name, $profile, $age, " years old!";
+    // echo "<br>";
+    // // $ret = echo $name;
+
+    // // echo $ret = print($name);
+
+    // print $name, $profile, $age, " years old!";
+
+    // Constant
+    // define("WELCOME", "KicsUET", true);
+    // echo welcome;
+    // define("HELLO", "KICSUET", true);
+    // echo hello, "</br>";
+
+    //globally
+    define("PI", "3.14");
+    function testGlobal(){
+        echo 'Circle Radius is ','', 2*PI*5;
+    }
+    // testGlobal();
+
+
+    // data types
+    $integer = 123;
+    var_dump($integer);
+    echo "<br>";
+ 
+    $float = 123.456;
+    var_dump($float);
+    echo "<br>";
+ $string = "This is String.";
+    var_dump($string);
+    echo "<br>";
+  
+    $bool = true;
+    var_dump($bool);
+    echo "<br>";
+     
+?>
